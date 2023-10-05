3f0dbecb12ed28226c99c55cdb6922008a180852
R-Kuehler <bobby.kuehler@gmail.com>
2023-10-05 16:28:53 -0700

    Update dragAndDrop.ts
    WOrking square ND CIRCLE, started triangel

--- components/sketches/dragAndDrop.ts
+++ components/sketches/dragAndDrop.ts
@@ -2,19 +2,28 @@ import { P5CanvasInstance } from "@p5-wrapper/react";
 
 export const dragandDropSketch = (p5: P5CanvasInstance) =>{
 
-    let bx1 = 0;
+  //square
+let bx1 = 1;
 let by1 = 200;
-let boxSize1 = 200;
+let boxSize1 = 100;
 let overBox1 = false;
 let locked1 = false as boolean;
 let overBox1hole = false;
 
 
-let bx2 = 200;
-let by2 = 0;
-let boxSize2 = 200;
+let bx2 = 160;
+let by2 = 250;
+let boxSize2 = 100;
 let overBox2 = false;
 let locked2 = false;
+let overBox2hole = false;
+
+let bx3 = 220;
+let by3 = 200;
+let boxSize3 = 100;
+let overBox3 = false;
+let locked3 = false;
+let overBox3hole = false;
 
 let xOffset = 0.0;
 let yOffset = 0.0;
@@ -27,9 +36,21 @@ p5.setup = ()=> {
 
 p5.draw = ()=>{
   p5.background(222);
-p5.rect(0,0,200,200);
-  
-   if (p5.mouseX > 0 && p5.mouseX < 130  && p5.mouseY > 0  && p5.mouseY < 130) 
+  //shape holes
+  //square
+  let c= p5.color(0,0,0);
+  p5.fill(c);
+  p5.strokeWeight(1);
+  p5.stroke(255,0,255);
+  p5.rect(0,0,100,100);
+  //circle
+  c= p5.color(0,0,0);
+  p5.fill(c);
+  p5.strokeWeight(1);
+  p5.stroke(255,0,255);
+  p5.circle(160,50,100);
+  //div
+   if (p5.mouseX > 0 && p5.mouseX < 90  && p5.mouseY > 0  && p5.mouseY < 90) 
   {overBox1hole = true;} 
   else 
   {overBox1hole = false;}
@@ -51,26 +72,51 @@ p5.rect(0,0,200,200);
     bx1 = 0;
     by1 = 0;
   }
-  
-  
-  let c= p5.color(255,255,0);
-   p5.fill(c);
-  p5.strokeWeight(1);
-  p5.stroke(255,0,255);
-  p5.rect(bx1, by1, boxSize1, boxSize1);
-  
-   if (p5.mouseX > bx2  && p5.mouseX < bx2 + boxSize2 && p5.mouseY > by2  && p5.mouseY < by2 + boxSize2) 
+//div
+  if (p5.mouseX > 110 && p5.mouseX < 210  && p5.mouseY > 0  && p5.mouseY < 80) 
+  {overBox2hole = true;} 
+  else 
+  {overBox2hole = false;}
+
+  if (p5.mouseX > bx2 - boxSize2/2 && p5.mouseX < bx2 + boxSize2/2 && p5.mouseY > by2 - boxSize2/2 && p5.mouseY < by2 + boxSize2/2) 
   {overBox2 = true;} 
   else 
   {overBox2 = false;}
+
+  if (bx2==160 && by2==50)
+  {
+    locked2 = false;
+    overBox2 = false; 
+    overBox2hole = true;
+  }
   
+  if(overBox2 == true && overBox2hole == true && locked2 == true)
+  {
+    bx2 = 160;
+    by2 = 50;
+  }
+  
+  //moving box bottom left
   c= p5.color(0,255,0);
    p5.fill(c);
-  p5.strokeWeight(5);
-  p5.stroke(0,0,255);
-  p5.rect(bx2, by2, boxSize2, boxSize2);
- 
+  p5.strokeWeight(1);
+  p5.stroke(255);
+  p5.rect(bx1, by1, boxSize1, boxSize1);
   
+  //moving circle
+  c= p5.color(255,0,0);
+   p5.fill(c);
+  p5.strokeWeight(1);
+  p5.stroke(255);
+  p5.circle(bx2, by2, boxSize2);
+ 
+  //moving triangle
+  c=p5.color(255,0,0);
+  p5.fill(c);
+  p5.strokeWeight(1);
+  p5.stroke(255);
+  p5.triangle(bx3, by3, bx3+100, by3+0, bx3+100, by3+100);
+
      if(overBox1 == true && overBox2 == true){
   overBox1 = false;
   overBox2 = true;
