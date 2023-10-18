a8d1129922f6606e96620a8ae9e06662434934fe
John Li (Tet) <tet@tet.moe>
2023-10-17 18:31:42 -0700

    Added a bit of commments

--- components/sketches/dragAndDrop.ts
+++ components/sketches/dragAndDrop.ts
@@ -52,6 +52,12 @@ class Box implements Shape {
     ShapeManager.getInstance().addShape(this);
   }
   id?: number | undefined;
+  /**
+   * Checks if the mouse (or the coordinates) is clicking on the shape.
+   * @param x the x coordinate of the mouse
+   * @param y the y coordinate of the mouse
+   * @returns a boolean indicating if the mouse is clicking on the shape
+   */
   isClickingOnShape(x: number, y: number) {
     return (
       x > this.x &&
@@ -60,11 +66,17 @@ class Box implements Shape {
       y < this.y + this.boxSize
     );
   }
+  /**
+   * Draws the shape.
+   */
   draw() {
     // console.log("drawing");
     this.drawHole();
     this.drawShape();
   }
+  /**
+   * Draws the hole, where the shape should be moved to.
+   */
   drawHole() {
     let c = this.p5.color(0, 0, 0);
     this.p5.fill(c);
@@ -77,6 +89,9 @@ class Box implements Shape {
       this.boxSize
     );
   }
+  /**
+   * Draws the shape.
+   */
   drawShape() {
     let c = this.correct ? this.p5.color(0, 255, 0) : this.p5.color(0, 0, 255);
     this.p5.fill(c);
@@ -84,6 +99,11 @@ class Box implements Shape {
     this.p5.stroke(255, 0, 255);
     this.p5.rect(this.x, this.y, this.boxSize, this.boxSize);
   }
+  /**
+   * Updates the position of the shape.
+   * @param newPosition the new position of the shape
+   * @param absolute if true, the shape will be moved to the new position. If false, the shape will be moved by the amount specified in newPosition
+   */
   updatePosition(newPosition: Coordinate, absolute?: boolean) {
     if (absolute) {
       this.x = newPosition.x;
@@ -96,23 +116,13 @@ class Box implements Shape {
       this.shapeWin();
     }
   }
+  /**
+   * Checks if the shape is in the hole.
+   * @param x the x coordinate of the shape
+   * @param y the y coordinate of the shape
+   * @returns true if the shape is in the hole, false otherwise
+   */
   checkForHole(x: number, y: number) {
-    // console.log(
-    //   "checking for hole",
-    //   `X tol`,
-    //   this.destination.x - this.boxSize * this.tolerance,
-    //   `X tol`,
-    //   this.destination.x + this.boxSize * this.tolerance,
-    //   `Y tol`,
-    //   this.destination.y - this.boxSize * this.tolerance,
-    //   `Y tol`,
-    //   this.destination.y + this.boxSize * this.tolerance,
-    //   { x, y },
-    //   x >= this.destination.x - this.boxSize * this.tolerance,
-    //   x <= this.destination.x + this.boxSize * this.tolerance,
-    //   y >= this.destination.y - this.boxSize * this.tolerance,
-    //   y <= this.destination.y + this.boxSize * this.tolerance
-    // );
     return (
       x >= this.destination.x - this.boxSize * this.tolerance &&
       x <= this.destination.x + this.boxSize * this.tolerance &&
@@ -120,6 +130,9 @@ class Box implements Shape {
       y <= this.destination.y + this.boxSize * this.tolerance
     );
   }
+  /**
+   * This function is called when the shape is in the correct position.
+   */
   shapeWin() {
     this.x = this.destination.x;
     this.y = this.destination.y;
