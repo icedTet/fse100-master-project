14327e507322ba913e894556e0515eb01501cefb
HKosten <146894895+HKosten@users.noreply.github.com>
2023-11-07 16:38:36 -0600

    push
    push

--- components/Games/LabyrinthEscape/Parts.ts
+++ components/Games/LabyrinthEscape/Parts.ts
@@ -1,1 +1,27 @@
+import { P5CanvasInstance } from "@p5-wrapper/react";
+import { Coordinate } from "./PlayerPositioning";
+
 //from shape manager
+class Parts{
+    x: number;
+    y: number;
+    len: number;
+    p5: P5CanvasInstance;
+
+    constructor(start: Coordinate, len: number, p5: P5CanvasInstance){
+        this.x = start.x;
+        this.y = start.y;
+        this.len = len;
+        this.p5 = p5;
+    }
+
+    draw(){
+        const path=[]
+        this.x-=20;
+        for(let i =0; i < length; i++){
+            this.x+=120;
+            const rect= this.p5.rect(this.x,this.y,120,120);
+            path.push(rect);
+        }
+    }
+}
