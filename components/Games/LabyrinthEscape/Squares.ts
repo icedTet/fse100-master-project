f11f9e185be120476ae225973f1434b5e701c9a3
HKosten <146894895+HKosten@users.noreply.github.com>
2023-11-07 16:16:36 -0600

    Create Squares.ts

---
+++ components/Games/LabyrinthEscape/Squares.ts
@@ -0,0 +1,19 @@
+import { P5CanvasInstance } from "@p5-wrapper/react";
+import { Coordinate } from "../DragAndDrop/dndTypes";
+
+class Square{
+    x: number;
+    y: number;
+    size: number;
+    p5: P5CanvasInstance;
+    constructor(start: Coordinate, size: number, p5: P5CanvasInstance){
+        this.x = start.x;
+        this.y = start.y;
+        this.size = size;
+        this.p5 = p5;
+    }
+
+    draw(){
+        
+    }
+}
