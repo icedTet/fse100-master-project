86e87adc95852fa24b95f7e869c5ea69e3c7d998
HKosten <146894895+HKosten@users.noreply.github.com>
2023-11-16 16:13:49 -0600

    Revert "Made Detection"
    This reverts commit 81d1391235d3f449a0924c77a46f392068666f62.

--- components/Games/LabyrinthEscape/Parts.ts
+++ components/Games/LabyrinthEscape/Parts.ts
@@ -3,37 +3,59 @@ import { Coordinate } from "./PlayerPositioning";
 import { Square } from "./Squares";
 
 //from shape manager
-export class Parts {
+export class Parts{
     x: number;
     y: number;
     len: number;
+    correct: boolean;
+    previousPart: Parts | null;
     p5: P5CanvasInstance;
-    squareNum: Square[]
-    constructor(start: Coordinate, len: number, p5: P5CanvasInstance) {
+    squares: Square[]
+    constructor(start: Coordinate, len: number, previousPart: Parts | null, p5: P5CanvasInstance){
         this.x = start.x;
         this.y = start.y;
         this.len = len;
+        this.correct = false;
+        this.previousPart = previousPart;
         this.p5 = p5;
-        this.squareNum = [];
+        this.squares= [];
         this.init()
     }
 
-    draw() {
+    draw(){
         console.log("draw")
         this.drawPart();
 
     }
-    drawPart() {
-        this.squareNum.forEach(sq => {
+    drawPart(){
+        this.squares.forEach(sq=>{
             sq.draw()
         })
     }
-    init() {
-
-        for (let i = 0; i < this.len; i++) {
-            const square = new Square({ x: this.x + i * 120, y: this.y }, 120, this.p5);
-            this.squareNum.push(square);
+    init(){
+        let previousSquare = null;
+        if(this.previousPart==null){
+        let previousSquare= new Square({x: this.x+0*120, y: this.y}, 120, null, this.p5);
+        this.squares.push(previousSquare);
         }
-       
+        else{
+            let previousSquare= new Square({x: this.x+0*120, y: this.y}, 120, this.previousPart.squares[this.previousPart.len], this.p5);
+            this.squares.push(previousSquare);
+        }
+
+        for(let i =1; i < this.len; i++){
+            let s= null;
+            s = new Square({x: this.x+i*120, y: this.y}, 120, previousSquare, this.p5);
+            this.squares.push(s);
+            previousSquare = s;
+        }
+    }
+    partCompleted(x: number, y: number){
+        for(let i = 0; i < this.squares.length; i++){
+        if(!this.squares[i].correct)
+            return false;
+        }
+            this.correct = true;
+            return true;
     }
 }
