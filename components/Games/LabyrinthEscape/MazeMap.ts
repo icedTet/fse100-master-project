c5d3dcf81eda275aa6129efb449d45ecb101f604
HKosten <146894895+HKosten@users.noreply.github.com>
2023-11-22 10:24:47 -0600

    tweaks

--- components/Games/LabyrinthEscape/MazeMap.ts
+++ components/Games/LabyrinthEscape/MazeMap.ts
@@ -19,74 +19,98 @@ export class MazeMap{
       this.mode = mode
       
       if(this.mode == 1){
-        this.leftToRightPathDrawer(0,0,11)
-        this.topToBottomPathDrawer(1200,120,4)
+        this.right(11)
+        this.down(4)
       }
       else if(this.mode == 2){
-        this.topToBottomPathDrawer(0,0,3)
-        this.leftToRightPathDrawer(120,240,6)
-        this.bottomToTopPathDrawer(720,120,2)
-        this.leftToRightPathDrawer(840,0,2)
-        this.topToBottomPathDrawer(960,120,4)
-        this.leftToRightPathDrawer(1080,480,2)
+        this.down(3)
+        this.right(6)
+        this.up(2)
+        this.right(2)
+        this.down(4)
+        this.right(2)
       }
       else{
-      this.leftToRightPathDrawer(0,0,3)
-      this.topToBottomPathDrawer(240,120,2)
-      this.rightToLeftPathDrawer(120,240,2)
-      this.topToBottomPathDrawer(0,360,2)
-      this.leftToRightPathDrawer(120,480,6)
-      this.bottomToTopPathDrawer(720,360,2)
-      this.rightToLeftPathDrawer(600,240,2)
-      this.bottomToTopPathDrawer(480,120,2)
-      this.leftToRightPathDrawer(600,0,6)
-      this.topToBottomPathDrawer(1200,120,2)
-      this.rightToLeftPathDrawer(1080,240,2)
-      this.topToBottomPathDrawer(960,360,2)
-      this.leftToRightPathDrawer(1080,480,2)
+        this.right(3)
+        this.down(2)
+        this.left(2)
+        this.down(2)
+        this.right(6)
+        this.up(2)
+        this.left(2)
+        this.up(2)
+        this.right(6)
+        this.down(2)
+        this.left(2)
+        this.down(2)
+        this.right(2)
       }
     }
 
-    leftToRightPathDrawer(xVal: number, yVal: number, len: number){
+    right(len: number){
+      let xVal = 0;
+      let yVal = 0;
+      if(this.squares.length!=0){
+        xVal = this.squares[this.squares.length-1].x+120
+        yVal = this.squares[this.squares.length-1].y
+      }
       for(let i = 0; i < len; i++){
         if(this.squares.length == 0)
         this.squares.push(new Square({x: i*120+xVal, y: yVal},120, null, this.p5))
         else
         this.squares.push(new Square({x: i*120+xVal, y: yVal},120, this.squares[this.squares.length-1], this.p5))
-        //console.log(this.squares[i].x + ", " + this.squares[i].y)
+        console.log(this.squares[i].x + ", " + this.squares[i].y)
         this.len++
       }
     }
 
-    rightToLeftPathDrawer(xVal: number, yVal: number, len: number){
+    left(len: number){
+      let xVal = 0;
+      let yVal = 0;
+      if(this.squares.length!=0){
+        xVal = this.squares[this.squares.length-1].x-120
+        yVal = this.squares[this.squares.length-1].y
+      }
       for(let i = 0; i < len; i++){
         if(this.squares.length == 0)
         this.squares.push(new Square({x: -i*120+xVal, y: yVal},120, null, this.p5))
         else
         this.squares.push(new Square({x: -i*120+xVal, y: yVal},120, this.squares[this.squares.length-1], this.p5))
-        //console.log(this.squares[i].x + ", " + this.squares[i].y)
+        console.log(this.squares[i].x + ", " + this.squares[i].y)
         this.len++
       }
     }
 
-    topToBottomPathDrawer(xVal: number, yVal: number, len: number){
+    down(len: number){
+      let xVal = 0;
+      let yVal = 0;
+      if(this.squares.length!=0){
+        xVal = this.squares[this.squares.length-1].x
+        yVal = this.squares[this.squares.length-1].y+120
+      }
       for(let i = 0; i < len; i++){
         if(this.squares.length == 0)
         this.squares.push(new Square({x: xVal, y: i*120+yVal},120, null, this.p5))
         else
         this.squares.push(new Square({x: xVal, y: i*120+yVal},120, this.squares[this.squares.length-1], this.p5))
-        //console.log(this.squares[i].x + ", " + this.squares[i].y)
+        console.log(this.squares[i].x + ", " + this.squares[i].y)
         this.len++
       }
     }
 
-    bottomToTopPathDrawer(xVal: number, yVal: number, len: number){
+    up(len: number){
+      let xVal = 0;
+      let yVal = 0;
+      if(this.squares.length!=0){
+        xVal = this.squares[this.squares.length-1].x
+        yVal = this.squares[this.squares.length-1].y-120
+      }
       for(let i = 0; i < len; i++){
         if(this.squares.length == 0)
         this.squares.push(new Square({x: xVal, y: -i*120+yVal},120, null, this.p5))
         else
         this.squares.push(new Square({x: xVal, y: -i*120+yVal},120, this.squares[this.squares.length-1], this.p5))
-        //console.log(this.squares[i].x + ", " + this.squares[i].y)
+        console.log(this.squares[i].x + ", " + this.squares[i].y)
         this.len++
       }
     }
